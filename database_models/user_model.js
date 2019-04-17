1aabb750edbcfe44dde92cb8af2fec28dea38cc8
jmoon9 <47996673+jmoon9@users.noreply.github.com>
2019-04-17 04:38:12 -0400

    profile view and data model update

--- database_models/user_model.js
+++ database_models/user_model.js
@@ -2,20 +2,34 @@ const mongoose = require("mongoose");
 const shortid = require("shortid");
 
 var userProfileSchema = new mongoose.Schema({
-    location: {type:String, default: "None"},
-    description: {type: String, default: "None"},
-    interests: {type: String, default: "None"},
+    location: {type: String, default: ""},
+    bio: {type: String, default: ""},
     profile_pic: {type: String, default: "default_profile.png"}
+})
+
+var walletSchema = new mongoose.Schema({
+    pubkey: {type: String, default: ""},
+    balance: {type: Number, default: 0}
 })
 
 var userSchema = new mongoose.Schema({
     name: {type:String},
+    username: {type:String},
     email: {type:String},
     password: {type:String},
     member_id: {type:String, default: shortid.generate},
     friends: [{"member_id":String, "friend_name":String, "profile_pic":String}],
     friend_requests:[{"member_id":String, "friend_name":String, "profile_pic":String}],
-    user_profile:[userProfileSchema] //sub document
+
+    wins: {type:Number, default: 0},
+    losses: {type:Number, default: 0},
+
+    open_bets: [{"challenger_id":String, "challenger_name":String, "mediator_id":String, "amount_to_win":Number, "amount_to_pay":Number}],
+    closed_bets: [{"challenger_id":String, "challenger_name":String, "profile_pic":String, "win":Boolean, "amount_paid":Number, bet_data: {type: Date, default: Date.now}}],
+    requests: [{"challenger_id":String, "challenger_name":String, "amount_to_win":Number, "amount_to_pay":Number}],
+
+    user_profile:[userProfileSchema],
+    wallet: [walletSchema]//sub document
 })
 
 var User = mongoose.model("User", userSchema);
--- routes/profile/index.js
+++ routes/profile/index.js
@@ -1,4 +1,5 @@
 const User = require("../../database_models/user_model");
+const shortid = require("shortid");
 
 module.exports.plugin = {
     pkg: require("./package.json"),
@@ -10,8 +11,16 @@ module.exports.plugin = {
                 config: {
                     auth: "session",
                     handler: async function(request, h){
-                        var users = await User.find({"email": {$ne: request.auth.credentials.user}}).exec();
-                        return h.view("friends", {user_friends: users});
+                        var user = await User.findOne({"email": request.auth.credentials.user}).exec();
+                        if(!user.user_profile[0]){
+                            user.user_profile.push({});
+                            await user.user_profile[0].save();
+                        }
+                        if(!user.wallet[0]){
+                            user.wallet.push({});
+                            await user.wallet[0].save();
+                        }
+                        return h.view("profile", {user: user});
                     }
                 }
             }
